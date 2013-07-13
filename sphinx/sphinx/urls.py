3d8ef3140d18b1dc86e46d3b8f3ef46e72d7560f
huahu <action0324@gmail.com>
2013-07-13 18:37:53 +0800

    add gitignore

--- sphinx/sphinx/urls.py
+++ sphinx/sphinx/urls.py
@@ -14,5 +14,6 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
+    # url(r'^admin/', include(admin.site.urls)),
     url(r'^index/$', 'note.views.index'),
 )
