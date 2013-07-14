969e660a9856e5afc97c8561d15acd977ad7151f
billychou <zsc1528@gmail.com>
2013-07-14 05:31:58 -0700

    add views test

--- sphinx/note/views.py
+++ sphinx/note/views.py
@@ -1,5 +1,10 @@
-from django.http import HttpResponse,HttpResponseRedirect
+#-*- coding:utf-8 -*-
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts  import render_to_response
+from django.template import loader, Context
 
 def index(req):
+    """
+        Define the index request 
+    """
     return HttpResponse('<h1>Hello World</h1>')
--- sphinx/sphinx/urls.py
+++ sphinx/sphinx/urls.py
@@ -10,9 +10,10 @@ urlpatterns = patterns('',
     # url(r'^sphinx/', include('sphinx.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
     url(r'^index/$', 'note.views.index'),
+    url(r'^$', 'note.views.index'),
     )
