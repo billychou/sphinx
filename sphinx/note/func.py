ffc6279af37fc13de6ed10effd853902d45097ea
huahu <action0324@gmail.com>
2013-07-27 01:32:32 +0800

    Change test

--- sphinx/note/func.py
+++ sphinx/note/func.py
@@ -1,7 +1,8 @@
 #-*- coding:utf-8 -*-
-
 import string 
-import random 
+import random
+
+
 def generatestr():
     """ 
         Func: generateStr
@@ -11,10 +12,10 @@ def generatestr():
     """
     seed_str = string.lowercase
     seed_digits = string.digits
-    aStr = ''
-    tNum = ''
-    lenStr = random.randint(6,10)
-    aStr += ''.join( random.choice(seed_str) for i in xrange(lenStr) )
-    tNum += ''.join( random.choice(seed_digits) for i in xrange(2) )
-    return aStr+tNum
+    astr = ''
+    tnum = ''
+    lenstr = random.randint(6,10)
+    astr += ''.join( random.choice(seed_str) for i in xrange(lenstr) )
+    tnum += ''.join( random.choice(seed_digits) for i in xrange(2) )
+    return astr+tnum
 
