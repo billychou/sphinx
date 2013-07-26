55bc50616c8d276386a064fe3c9d0e2c6c4c6a98
root <root@localhost.localdomain>
2013-07-26 13:03:30 +0800

    update the form

--- sphinx/note/forms.py
+++ sphinx/note/forms.py
@@ -3,5 +3,5 @@ from note.models import Notepad
 from django import forms
 
 class NotepadForm(forms.Form):
-    text = forms.CharField(label="Text")
+    text=forms.CharField(label="Text", widget=forms.Textarea(attrs={'style':'width:100%;height:30em'}))
 
