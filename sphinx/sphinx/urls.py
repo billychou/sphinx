553265a18c74943a218b022cbdb48bdfe39a00ed
zsc1528 <651265044@qq.com>
2013-08-01 21:50:46 +0800

    更改urls.py中不用注释

--- sphinx/sphinx/urls.py
+++ sphinx/sphinx/urls.py
@@ -6,14 +6,8 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'sphinx.views.home', name='home'),
-    # url(r'^sphinx/', include('sphinx.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
     url(r'^index/$', 'note.views.index'),
     url(r'^$', 'note.views.index'),
